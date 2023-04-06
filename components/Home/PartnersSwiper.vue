0544e45b4e0b5f0cddfd358331fcd5a15f338297
tzhovtyi <tzzzhovtyy@gmail.com>
2023-04-07 01:06:28 +1000

    Finalize swiper

--- components/Home/PartnersSwiper.vue
+++ components/Home/PartnersSwiper.vue
@@ -9,37 +9,23 @@
             slides-per-view="2"
             loop
             :breakpoints="{
+                370: {
+                    slidesPerView: 2,
+                },
                 768: {
-                    slidesPerView: 3,
+                    slidesPerView: 4,
                 },
                 1200: {
-                    slidesPerView: 4,
+                    slidesPerView: 5,
                 },
             }"
         >
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test1.png"
-            /></SwiperSlide>
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test2.png" />
-            </SwiperSlide>
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test3.png" />
-            </SwiperSlide>
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test3.png" />
-            </SwiperSlide>
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test3.png" />
-            </SwiperSlide>
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test3.png" />
-            </SwiperSlide>
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test3.png" />
-            </SwiperSlide>
-            <SwiperSlide class="partner"
-                ><img src="@/public/images/test3.png" />
+            <SwiperSlide
+                v-for="partner in partnersList"
+                :key="partner"
+                class="partner"
+            >
+                <img :src="partner" />
             </SwiperSlide>
             <HomeSwiperControls />
         </Swiper>
@@ -47,7 +33,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+    computed: {
+        partnersList() {
+            return [
+                '/_nuxt/assets/icons/partners/partner1.svg',
+                '/_nuxt/assets/icons/partners/partner2.svg',
+                '/_nuxt/assets/icons/partners/partner3.svg',
+                '/_nuxt/assets/icons/partners/partner4.svg',
+                '/_nuxt/assets/icons/partners/partner5.svg',
+                '/_nuxt/assets/icons/partners/partner6.svg',
+                '/_nuxt/assets/icons/partners/partner7.svg',
+                '/_nuxt/assets/icons/partners/partner8.svg',
+                '/_nuxt/assets/icons/partners/partner9.svg',
+                '/_nuxt/assets/icons/partners/partner10.svg',
+                '/_nuxt/assets/icons/partners/partner11.svg',
+            ];
+        },
+    },
+};
 </script>
 <style
     src="@/assets/scss/components/Home/PartnersSwiper.scss"
