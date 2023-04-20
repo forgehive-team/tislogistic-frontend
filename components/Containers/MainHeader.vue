901d6d2c56072a97aa3837fc6108f4b01da06200
tzhovtyi <tzzzhovtyy@gmail.com>
2023-04-20 10:12:57 +1000

    Hot fix

--- components/Containers/MainHeader.vue
+++ components/Containers/MainHeader.vue
@@ -6,11 +6,11 @@
             <br class="mobile-tablet-only" />
             <span>{{ $texts.companyName }}</span>
         </h1>
-        <div
-            class="containers-header__btns"
-            @click="calculatorPopupShown = !calculatorPopupShown"
-        >
-            <button class="animate-red">
+        <div class="containers-header__btns">
+            <button
+                class="animate-red"
+                @click="calculatorPopupShown = !calculatorPopupShown"
+            >
                 {{ $texts.containersCalculate }}
             </button>
             <button class="animate-yellow-btn containers-header__btn_white">
