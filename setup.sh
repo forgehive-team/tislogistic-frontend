62dbf3e196b2dbdb26a3514709373fc7dd92e1e6
nikitamakovey <nikitamakovey@yahoo.com>
2023-05-19 23:49:39 +0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cp -rf ../tis-legacy .
+git fetch origin main
 
-docker-compose up --build --remove-orphans -d proxy
+git reset --hard FETCH_HEAD
+
+sudo chmod -R 777 proxy
+
+docker-compose up -d --build frontend
+
+docker-compose restart proxy
+
+docker image prune -f
+
+docker builder prune -f
