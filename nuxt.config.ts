df4a682cf51caa1cdc9c0cfd3fbff3b4e644409d
tzhovtyi <tzzzhovtyy@gmail.com>
2023-06-07 04:05:14 +1000

    Test: deter jivo script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,6 +57,7 @@ export default defineNuxtConfig({
                 {
                     src: '//code-ya.jivosite.com/widget/e0QPdWruZj',
                     async: true,
+                    body: true,
                 },
             ],
         },
