d97a4b6c682d6628a88133a5d9e4b8316ac6efaa
tzhovtyi <tzzzhovtyy@gmail.com>
2023-08-01 23:35:54 +0300

    Connect cities

--- components/Home/BranchesList.vue
+++ components/Home/BranchesList.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="branches">
         <h2 class="branches__title">{{ $texts.branchesTitle }}</h2>
-        <div class="branches__wrapper">
+        <div v-if="items.length" class="branches__wrapper">
             <HomeBranchesItem
-                v-for="item in branchesList"
-                :key="item.city"
-                :city="item.city"
-                :phone="item.phone"
+                v-for="(item, i) in items"
+                :key="i"
+                :city="item.name"
+                :phone="item.phone_number"
                 :email="item.email"
             />
         </div>
@@ -18,43 +18,14 @@
 
 <script setup>
 const { $texts } = useNuxtApp();
-const branchesList = [
-    {
-        city: $texts.moscow,
-        phone: $texts.moscowPhone,
-        email: $texts.moscowEmail,
-    },
-    {
-        city: $texts.spb,
-        phone: $texts.spbPhone,
-        email: $texts.spbEmail,
-    },
-    {
-        city: $texts.khabarovsk,
-        phone: $texts.khabarovskPhone,
-        email: $texts.khabarovskEmail,
-    },
-    {
-        city: $texts.nakhodka,
-        phone: $texts.nakhodkaPhone,
-        email: $texts.nakhodkaEmail,
-    },
-    {
-        city: $texts.vladivostok,
-        phone: $texts.vladivostokPhone,
-        email: $texts.vladivostokEmail,
-    },
-    {
-        city: $texts.novosibirsk,
-        phone: $texts.novosibirskPhone,
-        email: $texts.novosibirskEmail,
-    },
-    {
-        city: $texts.ussuriysk,
-        phone: $texts.ussuriyskPhone,
-        email: $texts.ussuriyskEmail,
-    },
-];
+const { newsApiBase } = useRuntimeConfig();
+const url = newsApiBase + 'cities';
+const items = ref([]);
+
+onMounted(async () => {
+    const data = await $fetch(url);
+    items.value = data;
+});
 </script>
 
 <style
