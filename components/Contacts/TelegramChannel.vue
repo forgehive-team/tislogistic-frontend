e84a9dbbfd11b6c26f91ebb88c93761ba6ac4025
tzhovtyi <tzzzhovtyy@gmail.com>
2023-08-31 21:10:45 +0300

    Fix tg link

--- components/Contacts/TelegramChannel.vue
+++ components/Contacts/TelegramChannel.vue
@@ -2,7 +2,7 @@
     <div class="telegram">
         <h2>{{ $texts.telegramTitle }}</h2>
         <p>{{ $texts.telegramDescription }}</p>
-        <HomeLinkUnderline :to="$texts.telegramLink">{{
+        <HomeLinkUnderline :to="$texts.telegramLink" :blank="true">{{
             $texts.goToTelegram
         }}</HomeLinkUnderline>
         <nuxt-img src="images/telegram.png" format="webp" quality="100" />
