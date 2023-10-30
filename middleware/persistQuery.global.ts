3209bd2b3daf22542ceb4e24675e0711d786347c
tzhovtyi <tzzzhovtyy@gmail.com>
2023-10-30 17:06:27 +0300

    Add 404 redirect

--- middleware/persistQuery.global.ts
+++ middleware/persistQuery.global.ts
@@ -1,4 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+    const router = useRouter();
+    const route = router.resolve(to.path);
+    if (!route.matched.length) {
+        return navigateTo('/');
+    }
     if (Object.keys(from.query).length && !Object.keys(to.query).length) {
         return navigateTo({
             path: to.path,
