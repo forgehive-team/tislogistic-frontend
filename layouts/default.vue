57f8f10dd1905d8f0b2017ae4b4f18de20986d04
tzhovtyi <tzzzhovtyy@gmail.com>
2023-12-10 15:07:09 +0300

    Add tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,6 +28,19 @@ export default {
     setup() {
         const isWhiteTheme = useTheme();
         useHead({
+            script: [
+                {
+                    children:
+                        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-59B7F22B');",
+                },
+            ],
+            noscript: [
+                {
+                    tagPosition: 'bodyOpen',
+                    innerHTML:
+                        '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59B7F22B"height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+                },
+            ],
             bodyAttrs: {
                 class: computed(() => {
                     return isWhiteTheme.value ? 'white-theme' : 'dark-theme';
