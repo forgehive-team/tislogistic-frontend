d2dafb05091781849d6f81eed738bfbf955ab608
nikitamakovey <nikitamakovey@yahoo.com>
2024-04-17 12:32:07 +0700

    Add terminal services page

---
+++ components/TerminalnieUslugi/Advantages.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="car-advantages">
+        <h2>{{ $texts.terminalServicesAdvantages }}</h2>
+        <div class="car-advantages__container">
+            <div class="car-advantages__main-card">
+                <h3>{{ mainCard.title }}</h3>
+                <p>{{ mainCard.text }}</p>
+                <nuxt-img
+                    src="images/terminal_services/truck.png"
+                    :alt="$texts.terminalServicesAdvantagesSeo"
+                />
+            </div>
+            <HomeCardsItem
+                v-for="(advantage, i) in simpleCards"
+                :key="i"
+                :title="advantage.title"
+                :text="advantage.text"
+                class="_car"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import { advantages } from '@/config/terminalServicesList';
+
+export default {
+    computed: {
+        mainCard() {
+            return advantages[0];
+        },
+        simpleCards() {
+            return advantages.slice(-4);
+        },
+    },
+};
+</script>
+
+<style
+    src="@/assets/scss/components/AvtomobilnyePerevozki/CarAdvantages.scss"
+    lang="scss"
+    scoped
+></style>
---
+++ components/TerminalnieUslugi/AdvantagesSlider.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="advantages-slider">
+        <h2>{{ $texts.terminalServicesSliderTitle }}</h2>
+        <SharedSwiperControls
+            class="controls-desktop"
+            @slide-next="slideNext"
+            @slide-prev="slidePrev"
+        />
+        <Swiper
+            class="swiper"
+            :speed="600"
+            slides-per-view="1"
+            loop
+            @swiper="onSwiper"
+        >
+            <SwiperSlide v-for="(slide, i) in slides" :key="i" class="slide">
+                <HomeAdvantagesSliderSlide
+                    :img="slide.img"
+                    :title="slide.title"
+                    :subtitle="slide.subtitle"
+                />
+            </SwiperSlide>
+        </Swiper>
+        <SharedSwiperControls
+            class="controls-mobile"
+            @slide-next="slideNext"
+            @slide-prev="slidePrev"
+        />
+    </div>
+</template>
+
+<script setup>
+import { advantagesSlidesList as slides } from '~~/config/terminalServicesList';
+
+const swiperInstance = ref(null);
+let intervalRef;
+
+const onSwiper = (swiper) => {
+    swiperInstance.value = swiper;
+    intervalRef = setInterval(() => {
+        slideNext();
+    }, 7000);
+};
+const slideNext = () => {
+    swiperInstance.value?.slideNext();
+};
+const slidePrev = () => {
+    swiperInstance.value?.slidePrev();
+};
+onUnmounted(() => {
+    if (intervalRef) clearInterval(intervalRef);
+});
+</script>
+
+<style
+    src="@/assets/scss/components/Home/AdvantagesSlider.scss"
+    lang="scss"
+    scoped
+></style>
