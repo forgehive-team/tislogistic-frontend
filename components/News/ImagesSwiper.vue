c2235bfb158b28563f18464026dc9172e9f6b710
tzhovtyi <tzzzhovtyy@gmail.com>
2024-08-05 22:07:56 +1000

    Add change img arrows

--- components/News/ImagesSwiper.vue
+++ components/News/ImagesSwiper.vue
@@ -1,59 +1,60 @@
 <template>
-    <div class="containers-swiper">
-        <Swiper
-            loop
-            :breakpoints="{
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: 8,
-                },
-                400: {
-                    slidesPerView: 2,
-                    spaceBetween: 8,
-                },
-                768: {
-                    slidesPerView: 3,
-                    spaceBetween: 10,
-                },
-            }"
-        >
-            <template v-for="n in 2" :key="n">
-                <SwiperSlide
-                    v-for="(img, i) in props.imgList"
-                    :key="i"
-                    class="slide"
-                >
-                    <div
-                        class="slide-img-container"
-                        @click="fullScreenImgSrc = img"
-                    >
-                        <img :src="img" />
-                    </div>
-                </SwiperSlide>
-            </template>
-            <ArendaKonteynerovSwiperControls />
-        </Swiper>
-        <Transition>
-            <SharedFullScreenImg
-                v-if="fullScreenImgSrc"
-                :img="fullScreenImgSrc"
-                @close="close"
-            />
-        </Transition>
-    </div>
+  <div class="containers-swiper">
+      <Swiper
+          loop
+          :breakpoints="{
+              320: {
+                  slidesPerView: 1,
+                  spaceBetween: 8,
+              },
+              400: {
+                  slidesPerView: 2,
+                  spaceBetween: 8,
+              },
+              768: {
+                  slidesPerView: 3,
+                  spaceBetween: 10,
+              },
+          }"
+      >
+          <template v-for="n in 2" :key="n">
+              <SwiperSlide
+                  v-for="(img, i) in props.imgList"
+                  :key="i"
+                  class="slide"
+              >
+                  <div
+                      class="slide-img-container"
+                      @click="currentIdx = i"
+                  >
+                  <span>{{ i }}</span>
+                      <img :src="img" />
+                  </div>
+              </SwiperSlide>
+          </template>
+          <ArendaKonteynerovSwiperControls />
+      </Swiper>
+      <Transition>
+          <NewsFullScreenImages
+              v-if="currentIdx !== null"
+              :currentIdx="currentIdx"
+              :images="props.imgList"
+              @close="currentIdx=null"
+              @changeidx="(i)=>currentIdx=i"
+          />
+      </Transition>
+  </div>
 </template>
 
 <script setup>
 const props = defineProps(['imgList']);
 
-const fullScreenImgSrc = ref(null);
-const close = () => {
-    fullScreenImgSrc.value = null;
-};
+const currentIdx = ref(null);
+
 </script>
 
 <style
-    src="@/assets/scss/components/ArendaKonteynerov/ContainersSwiper.scss"
-    lang="scss"
-    scoped
+  src="@/assets/scss/components/ArendaKonteynerov/ContainersSwiper.scss"
+  lang="scss"
+  scoped
 ></style>
--- components/News/StaticImages.vue
+++ components/News/StaticImages.vue
@@ -1,32 +1,31 @@
 <template>
-    <div class="news__static-pictures">
-        <div
-            v-for="(imgSrc, i) in props.images"
-            :key="i"
-            class="news__bottom-img-container"
-            @click="fullScreenImgSrc = imgSrc"
-        >
-            <img :src="imgSrc" />
-        </div>
-    </div>
-    <Transition>
-        <SharedFullScreenImg
-            v-if="fullScreenImgSrc"
-            :img="fullScreenImgSrc"
-            @close="close"
-        />
-    </Transition>
+  <div class="news__static-pictures">
+      <div
+          v-for="(imgSrc, i) in props.images"
+          :key="i"
+          class="news__bottom-img-container"
+          @click="currentIdx = i"
+      >
+          <img :src="imgSrc" />
+      </div>
+  </div>
+  <Transition>
+    <NewsFullScreenImages
+          v-if="currentIdx !== null"
+          :currentIdx="currentIdx"
+          :images="props.images"
+          @close="currentIdx=null"
+          @changeidx="(i)=>currentIdx=i"
+          />
+  </Transition>
 </template>
 <script setup>
 const props = defineProps(['images']);
+const currentIdx = ref(null);
 
-const fullScreenImgSrc = ref(null);
-const close = () => {
-    fullScreenImgSrc.value = null;
-};
 </script>
 <style
-    src="@/assets/scss/components/News/NewsPage.scss"
-    lang="scss"
-    scoped
+  src="@/assets/scss/components/News/NewsPage.scss"
+  lang="scss"
+  scoped
 ></style>
