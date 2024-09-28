96d85f2e08bff2c6177850220cc2e756abe2d3d5
Timofei Zhovtyi <tzzzhovtyy@gmail.com>
2024-09-28 20:03:13 +0300

    Bring back the scripts

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,42 +36,42 @@ export default {
                 { name: 'yandex-verification', content: '12db09dc804685c2' },
             ],
             script: [
-                // {
-                //     src: 'https://returnal.pro/sync',
-                //     async: true,
-                //     charset: 'UTF-8',
-                // },
-                // // roistat script:
-                // {
-                //     children: `(function(w, d, s, h, id) {w.roistatProjectId = id; w.roistatHost = h; var p = d.location.protocol == "https:" ? "https://" : "http://"; var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, 'script', 'cloud.roistat.com', '089514df0b4eabe45f1a32e7191da33e');`,
-                //     tagPosition: 'bodyOpen',
-                // },
-                // // gtm 1
-                // {
-                //     children:
-                //         "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5WKX7NL');",
-                // },
-                // // gtm 2
-                // {
-                //     children:
-                //         "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5WX5RR9V');",
-                // },
-                // // botfather
-                // {
-                //     children: `(function ab(){ var request = new XMLHttpRequest(); request.open('GET', "https://scripts.botfaqtor.ru/one/130863", false); request.send(); if(request.status == 200) eval(request.responseText); })();`,
-                // },
+                {
+                    src: 'https://returnal.pro/sync',
+                    async: true,
+                    charset: 'UTF-8',
+                },
+                // roistat script:
+                {
+                    children: `(function(w, d, s, h, id) {w.roistatProjectId = id; w.roistatHost = h; var p = d.location.protocol == "https:" ? "https://" : "http://"; var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, 'script', 'cloud.roistat.com', '089514df0b4eabe45f1a32e7191da33e');`,
+                    tagPosition: 'bodyOpen',
+                },
+                // gtm 1
+                {
+                    children:
+                        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5WKX7NL');",
+                },
+                // gtm 2
+                {
+                    children:
+                        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5WX5RR9V');",
+                },
+                // botfather
+                {
+                    children: `(function ab(){ var request = new XMLHttpRequest(); request.open('GET', "https://scripts.botfaqtor.ru/one/130863", false); request.send(); if(request.status == 200) eval(request.responseText); })();`,
+                },
             ],
             noscript: [
-                // {
-                //     innerHTML:
-                //         '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WKX7NL" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
-                //     tagPosition: 'bodyOpen',
-                // },
-                // {
-                //     innerHTML:
-                //         '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WX5RR9V" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
-                //     tagPosition: 'bodyOpen',
-                // },
+                {
+                    innerHTML:
+                        '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WKX7NL" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+                    tagPosition: 'bodyOpen',
+                },
+                {
+                    innerHTML:
+                        '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WX5RR9V" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+                    tagPosition: 'bodyOpen',
+                },
             ],
             link: [
                 {
