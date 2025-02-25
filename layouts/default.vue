e088ab1f7c6beb56e237136edb7cfe175bc2e140
Timofei Zhovtyi <tzzzhovtyy@gmail.com>
2025-02-25 22:39:26 +0300

    Delete script

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,11 +34,6 @@ export default {
                     children:
                         "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-59B7F22B');",
                 },
-                {
-                    src: 'https://returnal.pro/sync',
-                    async: true,
-                    charset: 'UTF-8',
-                },
                 // roistat script:
                 {
                     children: `(function(w, d, s, h, id) {w.roistatProjectId = id; w.roistatHost = h; var p = d.location.protocol == "https:" ? "https://" : "http://"; var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, 'script', 'cloud.roistat.com', '089514df0b4eabe45f1a32e7191da33e');`,
