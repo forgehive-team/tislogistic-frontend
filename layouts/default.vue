7fdcabdc9b62d5c650cf8baceb50d030a0161daa
Timofei Zhovtyi <tzzzhovtyy@gmail.com>
2025-05-18 23:49:26 +1000

    bring back roistat script

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,6 +32,10 @@ export default {
             ],
             script: [
                 // roistat script:
+                {
+                    children: `(function(w, d, s, h, id) {w.roistatProjectId = id; w.roistatHost = h; var p = d.location.protocol == "https:" ? "https://" : "http://"; var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, 'script', 'cloud.roistat.com', '089514df0b4eabe45f1a32e7191da33e');`,
+                    tagPosition: 'bodyOpen',
+                },
                 // botfather
                 {
                     children: `(function ab(){ var request = new XMLHttpRequest(); request.open('GET', "https://scripts.botfaqtor.ru/one/130863", false); request.send(); if(request.status == 200) eval(request.responseText); })();`,
