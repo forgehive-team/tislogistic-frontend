96f760b6a4aea2bae448fd0ea8bdab61bb02f3e8
Timofei <tzzzhovtyy@gmail.com>
2025-07-16 22:28:09 +1000

    remove data layers

--- components/Shared/ReturnCallForm.vue
+++ components/Shared/ReturnCallForm.vue
@@ -286,9 +286,6 @@ export default {
             this.successShown = !this.successShown;
             this.returnCallShown = false;
             try {
-                if (dataLayer) {
-                    dataLayer.push({ event: 'callback' });
-                }
                 await $fetch(`${apiBase}/api/callback_request/submit`, {
                     method: 'POST',
                     headers: {
